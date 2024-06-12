cbdbc0046d5473c0aabb829dd8942cbd7abe6813
Fadad Oussama <fadadoussama7@gmail.com>
2024-06-12 23:05:22 +0200

    Test page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center gap-x-2 bg-zinc-200 text-2xl">
+      problem
+      <span className="font-swearDisplay font-bold text-orange-500 underline underline-offset-[6px]">
+        solving
+      </span>
+    </div>
+  );
+}
