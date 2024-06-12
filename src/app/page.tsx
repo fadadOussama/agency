e09957ddd4a942b366f5ddb274dfc504d93de76a
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-12 21:49:06 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div className=" text-9xl">
-      <p>Services</p>
-      <p className="font-bold">Services</p>
-    </div>
-  );
+  return <></>;
 }
