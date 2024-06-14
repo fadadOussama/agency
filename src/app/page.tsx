9ba61e279dc7c0e71e6009065b2204fa30bce246
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-14 21:53:24 +0100

    add: grid pattern

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 export default function page() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center gap-x-2 bg-white text-2xl">
-      problem
-      <span className="text-orange font-swearDisplay font-bold underline underline-offset-[6px]">
-        solving
-      </span>
-    </div>
+    <></>
   );
 }
