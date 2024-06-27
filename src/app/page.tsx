a5149ec98820980bc3d32e8377e44cc2de40fc6f
Fadad Oussama <fadadoussama7@gmail.com>
2024-06-27 21:34:17 +0200

    create file note and work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import GridPattern from "@/components/magicui/grid-pattern";
+import { cn } from "@/lib/utils";
+
 export default function page() {
   return (
-    <></>
+    <GridPattern
+      width={30}
+      height={30}
+      x={-1}
+      y={-1}
+      className={cn(
+        "[mask-image:radial-gradient(circle_at_center,white_55%,transparent)]",
+      )}
+    />
   );
 }
