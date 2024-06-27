274d22e2bafc490e5a02966ce53dee40fb829e6f
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-27 21:17:47 +0100

    add: constant nav links

---
+++ src/constants/links.ts
@@ -0,0 +1,18 @@
+export const links: LinkType[] = [
+  {
+    name:'Services',
+    href:'#services'
+  },
+  {
+    name:'Work',
+    href:'#work'
+  },
+  {
+    name:'Why us',
+    href:'#why-us'
+  },
+  {
+    name:'Contact',
+    href:'#contact'
+  }
+]
