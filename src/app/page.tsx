cdc68a3734185f46f5ccb73122d8475ab49773ef
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-28 22:04:06 +0100

    update: remove hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-import Hero from "@/components/hero";
 
 export default function page() {
   return (
-    <div>
-      <Hero />
-      <div className="bg-green-500 h-screen ">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident dignissimos quis voluptatibus quasi repellat dolores recusandae eaque eum amet voluptate quo illo, et consequuntur officiis neque, libero dolor quod impedit.
-      </div>
-    </div>
+    <div>Coming soon</div>
   );
 }
