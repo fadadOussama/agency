2e5ef3cc11d7d725b88bb1cfc0310b6dca407a2b
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-28 22:15:22 +0100

    add: hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/hero";
+
 export default function page() {
   return (
-    <div className="grid h-screen w-full place-items-center font-swearDisplay text-9xl leading-none bg-ornge text-white">
-      Coming soon
+    <div>
+      <Hero />
     </div>
   );
 }
