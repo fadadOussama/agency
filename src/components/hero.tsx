2bd6d6d21a381eaffbd442cb96c3424822a6d9c5
Abdellah Idrissi <abdellahmoumenelidrissi@gmail.com>
2024-06-28 22:00:00 +0100

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -41,7 +41,6 @@ export default function Hero() {
           </div>
         </div>
 
-        {/* Hero text  */}
         <div>
           <p className="text-7xl font-bold leading-normal">
             Transforming ideas into{" "}
